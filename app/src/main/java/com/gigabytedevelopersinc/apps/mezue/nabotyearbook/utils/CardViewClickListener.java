71dbf03e426812bec8736e4539e2f00350898af8
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-06-08 18:20:47 +0100

    Dropped use of "Nabot Yearbook" as package Migrate to use of "Class of Champs 18" which translates to Class of Champions 2018 instead of Nabot Yearbook as used before
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/apps/mezue/nabotyearbook/utils/CardViewClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.gigabytedevelopersinc.apps.mezue.nabotyearbook.utils;
-
-import android.view.View;
-
-public interface CardViewClickListener {
-    void onItemClick(View view, int position);
-}
